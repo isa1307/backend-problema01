2f84396803d40ecc583c42c33425819a8bc11088
isakamitech <isabelasouzade.564@gmail.com>
2023-11-13 09:36:56 -0300

    teste de servidor ok

---
+++ src/routes/index.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const rotas = Router();
+
+rotas.get("/", (req, res) => {
+   return res.status(200).send(
+    {message :"Servidor ok!"});
+    });
+
+export default rotas;
